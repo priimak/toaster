4369143bf96e463736ef728943edecae6c053669
Dmitri Priimak <dpriimak@svbio.com>
2015-11-21 12:54:24 -0800

    reset version to 0.0.0-SNAPSHOT

--- src/main/java/net/priimak/toaster/Main.java
+++ src/main/java/net/priimak/toaster/Main.java
@@ -2,6 +2,6 @@ package net.priimak.toaster;
 
 public final class Main {
     public static void main(String[] cmd) {
-        System.out.print("I am a toaster");
+        System.out.println("I am a simple toaster in development.");
     }
 }
