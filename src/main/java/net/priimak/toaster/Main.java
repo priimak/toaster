b0399fb280e909de4fdda15f3530f8bdb9762736
Dmitri Priimak <dpriimak@svbio.com>
2015-11-21 12:57:06 -0800

    toaster is almost ready

--- src/main/java/net/priimak/toaster/Main.java
+++ src/main/java/net/priimak/toaster/Main.java
@@ -2,6 +2,6 @@ package net.priimak.toaster;
 
 public final class Main {
     public static void main(String[] cmd) {
-        System.out.println("I am almost a real toaster.");
+        System.out.println("I am a toaster candidate.");
     }
 }
